d676e964704cd1f5fd23d8865b53db039695f558
George Shibanin <george.shibanin@gmail.com>
2022-08-04 01:28:10 +0300

    final pattern2

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -3,10 +3,10 @@ package postgres
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgx/v4"
-
 	"github.com/GeorgeShibanin/Bwallgroup_test2/internal/storage"
+	"github.com/jackc/pgx/v4"
 	"github.com/pkg/errors"
+	"time"
 )
 
 const (
@@ -58,9 +58,31 @@ func (s *StoragePostgres) PutNewUser(ctx context.Context, client storage.Client,
 	if user.Id != 0 {
 		return storage.ErrAlreadyExist
 	}
+
+	tag, err := tx.Exec(ctx, InsertUserQuery, client, balance)
+	if err != nil {
+		return errors.Wrap(err, "can't insert link")
+	}
+
+	if tag.RowsAffected() != 1 {
+		return errors.Wrap(err, fmt.Sprintf("unexpected rows affected value: %v", tag.RowsAffected()))
+	}
+
+	return nil
 }
 
 func (s *StoragePostgres) GetBalance(ctx context.Context, user_id storage.Client) (storage.Balance, error) {
+	client := &Client{}
+	//получаем из базы значение по ключу
+	err := s.conn.QueryRow(ctx, GetUserByIDQuery, user_id).
+		Scan(&client.Id, &client.Balace)
+	if err != nil {
+		return 0, fmt.Errorf("something went wrong - %w", storage.StorageError)
+	}
+	return storage.Balance(client.Balace), err
+}
+
+func (s *StoragePostgres) PatchUserBalance(ctx context.Context, client storage.Client, balance storage.Balance) (storage.Balance, error) {
 	tx, err := s.conn.BeginTx(ctx, pgx.TxOptions{})
 	if err != nil {
 		return 0, errors.Wrap(err, "can't create tx")
@@ -72,8 +94,26 @@ func (s *StoragePostgres) GetBalance(ctx context.Context, user_id storage.Client
 			tx.Commit(ctx)
 		}
 	}()
-}
 
-func (s *StoragePostgres) PatchUserBalance(ctx context.Context, client storage.Client, balance storage.Balance) (storage.Balance, error) {
+	user := &Client{}
+	//получаем из базы значение по ключу
+	err = tx.QueryRow(ctx, GetUserByIDQuery, client).
+		Scan(&user.Id, &user.Balace)
+	if err != nil {
+		return 0, fmt.Errorf("something went wrong - %w", storage.StorageError)
+	}
 
+	current_balance := user.Balace
+	if current_balance+int(balance) < 0 {
+		return 0, fmt.Errorf("balance not enough - %w", storage.StorageError)
+	}
+	tag, err := tx.Exec(ctx, PutSummQuery, balance, false, time.Now())
+	if err != nil {
+		return 0, errors.Wrap(err, "can't update balance")
+	}
+
+	if tag.RowsAffected() != 1 {
+		return 0, errors.Wrap(err, fmt.Sprintf("unexpected rows affected value: %v", tag.RowsAffected()))
+	}
+	return storage.Balance(current_balance + int(balance)), nil
 }
